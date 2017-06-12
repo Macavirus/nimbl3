9d269a06e164b1c05d87e9be58eab79880d1b473
user <user@user.com>
2017-06-12 15:51:58 +0700

    Adds empty Orderlisting component

---
+++ src/components/Orderlisting.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="orderlisting">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'orderspec',
+  data () {
+    return {
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.orderlisting {
+  border: 1px solid rgba(201, 201, 201, 0.69);
+  border-radius: 4px;
+  background-color: #FFFFFF;
+  height: 400px;
+  margin-top: 17px;
+  margin-right: 17px;
+}
+</style>
